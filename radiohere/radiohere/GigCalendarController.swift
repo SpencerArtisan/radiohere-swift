c555ffe1690923905acdc53a47e53c3764c681c0
Spencer Ward <SpencerArtisan>
2014-12-25 19:25:40 +0100

    Move location logic into own controller

--- radiohere/radiohere/GigCalendarController.swift
+++ radiohere/radiohere/GigCalendarController.swift
@@ -10,111 +10,16 @@ import UIKit
 import CoreLocation
 
 
-class GigCalendarController: UIViewController, SRWebSocketDelegate, TSQCalendarViewDelegate, CLLocationManagerDelegate {
-    let SERVER = "ws://radiohere.herokuapp.com/game"
-    
-    var location: String?
-    var locationName: String?
-    var props: NSDictionary?
-    var socket: SRWebSocket?
-    var musicScene = MusicScene()
-    var locationManager = CLLocationManager()
-    var locationIndex = 0
-    var here: CLLocation!
-    var userLocations: [String] = []
-    
-    @IBOutlet weak var addButton: UIButton!
-    @IBOutlet weak var okButton: UIButton!
-    @IBOutlet weak var nameTextBox: UITextField!
-    @IBOutlet weak var locationLabel: UILabel!
-    @IBOutlet weak var nextButton: UIButton!
-    
-    
-    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
-        here = locations[0] as CLLocation
-    }
-    
-    @IBAction func nextLocation(sender: AnyObject) {
-        locationIndex = (locationIndex + 1) % userLocations.count
-        onLocationChange()
-    }
-    
-    @IBAction func addLocation(sender: AnyObject) {
-        nameTextBox.hidden = false
-        addButton.hidden = true
-        okButton.hidden = false
-        nextButton.hidden = true
-        nameTextBox.becomeFirstResponder()
-    }
-    
-    @IBAction func acceptLocation(sender: AnyObject) {
-        nameTextBox.hidden = true
-        addButton.hidden = false
-        nextButton.hidden = false
-        okButton.hidden = true
-        var locationString = "\(nameTextBox.text):\(here!.coordinate.latitude),\(here!.coordinate.longitude),5"
-        userLocations.append(locationString)
-        saveUserLocations()
-        onLocationChange()
-    }
-    
+class GigCalendarController: UIViewController, TSQCalendarViewDelegate {
+    var locationController = LocationController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        readUserLocations()
-        locationManager.delegate = self
-        locationManager.startUpdatingLocation()
         initCalendar()
-        onLocationChange()
+        locationController.viewDidLoad()
+        var timer = NSTimer.scheduledTimerWithTimeInterval(1.5, target: self, selector: Selector("updateTable"), userInfo: nil, repeats: true)
     }
     
-    func readUserLocations() {
-        var paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
-        var path = paths.stringByAppendingPathComponent("locations.plist")
-        var fileManager = NSFileManager.defaultManager()
-        if (!(fileManager.fileExistsAtPath(path))) {
-            var bundle : NSString = NSBundle.mainBundle().pathForResource("Data", ofType: "plist")!
-            fileManager.copyItemAtPath(bundle, toPath: path, error:nil)
-        }
-        
-//        data.setObject(object, forKey: "object")
-//        data.writeToFile(path, atomically: true)
-        
-        
-//        var paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
-//        var path = paths.stringByAppendingPathComponent("locations.plist")
-        props = NSDictionary(contentsOfFile: path)?.mutableCopy() as NSDictionary
-        
-        
-//        var path = NSBundle.mainBundle().pathForResource("Data", ofType: "plist")
-//        var props = NSDictionary(contentsOfFile: path!)
-        userLocations = props?.valueForKey("Locations") as [String]
-    }
-    
-    func saveUserLocations() {
-        var paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
-        var path = paths.stringByAppendingPathComponent("locations.plist")
-        props?.setValue(userLocations, forKey: "Locations")
-        props?.writeToFile(path, atomically: true)
-    }
-    
-
-    func onLocationChange() {
-        musicScene = MusicScene()
-        updateLocation()
-        closeWebSocket()
-        openWebSocket()
-        locationLabel.text = locationName
-    }
-    
-    func updateLocation() {
-        var locationDetails = userLocations[locationIndex]
-        let split = locationDetails.rangeOfString(":")!.startIndex
-        let name: String = locationDetails.substringWithRange(Range<String.Index>(start: locationDetails.startIndex, end: split))
-        location = locationDetails.substringWithRange(Range<String.Index>(start: split.successor(), end: locationDetails.endIndex))
-        locationName = name
-    }
     
     func initCalendar() {
         var calendar = TSQCalendarView()
@@ -129,9 +34,8 @@ class GigCalendarController: UIViewController, SRWebSocketDelegate, TSQCalendarV
         var modeBar = NSBundle.mainBundle().loadNibNamed("DateMode", owner: self, options: nil)[0] as UIView
         showBottomBar(modeBar)
 
-        var locationBar = NSBundle.mainBundle().loadNibNamed("LocationView", owner: self, options: nil)[0] as UIView
+        var locationBar = NSBundle.mainBundle().loadNibNamed("LocationView", owner: locationController, options: nil)[0] as UIView
         showTopBar(locationBar)
-        locationLabel.textColor = UIColor.innocence()
     }
     
     func showTopBar(view: UIView) {
@@ -157,52 +61,24 @@ class GigCalendarController: UIViewController, SRWebSocketDelegate, TSQCalendarV
         return true
     }
         
-    override func viewDidAppear(animated: Bool) {
-//        navigationController?.toolbarHidden = true
-    }
-
     @IBAction func onClickVenue(sender: AnyObject) {
         let secondViewController = self.storyboard?.instantiateViewControllerWithIdentifier("VenuesController") as VenuesController
-        secondViewController.musicScene = self.musicScene
+        secondViewController.musicScene = locationController.musicScene
     
         self.navigationController?.pushViewController(secondViewController, animated: true)
     }
     
     func calendarView(calendarView: TSQCalendarView, shouldSelectDate date: NSDate) -> Bool {
-        return musicScene.hasGigOn(date)
+        return locationController.getMusicScene().hasGigOn(date)
     }
 
     func calendarView(calendarView: TSQCalendarView, didSelectDate date: NSDate) {
         let secondViewController = self.storyboard?.instantiateViewControllerWithIdentifier("DetailViewController") as DetailViewController
-        secondViewController.gigs = self.musicScene.getGigsOn(date)
+        secondViewController.gigs = locationController.getMusicScene().getGigsOn(date)
         secondViewController.showByDate(date.format("EEE dd MMM"))
         self.navigationController?.pushViewController(secondViewController, animated: true)
     }
 
-    func openWebSocket() {
-        socket = SRWebSocket(URL: NSURL(string: SERVER))
-        socket?.delegate = self
-        socket?.open()
-    }
-    
-    func closeWebSocket() {
-        if (socket != nil) {
-            socket?.close()
-        }
-    }
-    
-    func webSocketDidOpen(socket: SRWebSocket!) {
-        println("Socket Open!")
-        socket.send(location)
-        var timer = NSTimer.scheduledTimerWithTimeInterval(1.5, target: self, selector: Selector("updateTable"), userInfo: nil, repeats: true)
-    }
-    
-    func webSocket(webSocket: SRWebSocket!, didReceiveMessage message: AnyObject) {
-        objc_sync_enter(self)
-        self.musicScene.add(Gig(jsonText: message.description))
-        objc_sync_exit(self)
-    }
-    
     func updateTable() {
         (self.view as TSQCalendarView).tableView.reloadData()
     }
---
+++ radiohere/radiohere/LocationController.swift
@@ -0,0 +1,135 @@
+//
+//  LocationController.swift
+//  radiohere
+//
+//  Created by Spencer Ward on 25/12/2014.
+//  Copyright (c) 2014 Spencer Ward. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationController: UIViewController, SRWebSocketDelegate, CLLocationManagerDelegate {
+    let SERVER = "ws://radiohere.herokuapp.com/game"    
+    
+    var socket: SRWebSocket?
+
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var okButton: UIButton!
+    @IBOutlet weak var nameTextBox: UITextField!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
+
+    var location: String?
+    var locationName: String?
+    var props: NSDictionary?
+    var userLocations: [String] = []
+    var locationIndex = 0
+    var locationManager = CLLocationManager()
+    var here: CLLocation!
+    var musicScene = MusicScene()
+
+    override func viewDidLoad() {
+        readUserLocations()
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        onLocationChange()
+        locationLabel.textColor = UIColor.innocence()
+    }
+
+    @IBAction func nextLocation(sender: AnyObject) {
+        locationIndex = (locationIndex + 1) % userLocations.count
+        onLocationChange()
+    }
+    
+    @IBAction func addLocation(sender: AnyObject) {
+        nameTextBox.hidden = false
+        addButton.hidden = true
+        okButton.hidden = false
+        nextButton.hidden = true
+        nameTextBox.becomeFirstResponder()
+    }
+    
+    @IBAction func acceptLocation(sender: AnyObject) {
+        nameTextBox.hidden = true
+        addButton.hidden = false
+        nextButton.hidden = false
+        okButton.hidden = true
+        var locationString = "\(nameTextBox.text):\(here!.coordinate.latitude),\(here!.coordinate.longitude),5"
+        userLocations.append(locationString)
+        saveUserLocations()
+        onLocationChange()
+    }
+    
+    func getMusicScene() -> MusicScene {
+        return musicScene
+    }
+    
+    func readUserLocations() {
+        var paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        var path = paths.stringByAppendingPathComponent("locations.plist")
+        var fileManager = NSFileManager.defaultManager()
+        if (!(fileManager.fileExistsAtPath(path))) {
+            var bundle : NSString = NSBundle.mainBundle().pathForResource("Data", ofType: "plist")!
+            fileManager.copyItemAtPath(bundle, toPath: path, error:nil)
+        }
+        
+        props = NSDictionary(contentsOfFile: path)?.mutableCopy() as NSDictionary
+        
+        
+        userLocations = props?.valueForKey("Locations") as [String]
+    }
+    
+    func saveUserLocations() {
+        var paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        var path = paths.stringByAppendingPathComponent("locations.plist")
+        props?.setValue(userLocations, forKey: "Locations")
+        props?.writeToFile(path, atomically: true)
+    }
+    
+    
+    func onLocationChange() {
+        musicScene = MusicScene()
+        updateLocation()
+        closeWebSocket()
+        openWebSocket()
+        locationLabel.text = locationName
+    }
+    
+    func updateLocation() {
+        var locationDetails = userLocations[locationIndex]
+        let split = locationDetails.rangeOfString(":")!.startIndex
+        let name: String = locationDetails.substringWithRange(Range<String.Index>(start: locationDetails.startIndex, end: split))
+        location = locationDetails.substringWithRange(Range<String.Index>(start: split.successor(), end: locationDetails.endIndex))
+        locationName = name
+    }
+    
+    func openWebSocket() {
+        socket = SRWebSocket(URL: NSURL(string: SERVER))
+        socket?.delegate = self
+        socket?.open()
+    }
+    
+    func closeWebSocket() {
+        if (socket != nil) {
+            socket?.close()
+        }
+    }
+    
+    func webSocketDidOpen(socket: SRWebSocket!) {
+        println("Socket Open!")
+        socket.send(location)
+    }
+    
+    func webSocket(webSocket: SRWebSocket!, didReceiveMessage message: AnyObject) {
+        objc_sync_enter(self)
+        self.musicScene.add(Gig(jsonText: message.description))
+        objc_sync_exit(self)
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        here = locations[0] as CLLocation
+    }
+    
+
+}
